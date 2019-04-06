0b38d5076ad439b6fbf0b14b0f5535ae3b7992bb
Tarasov Evgeniy <tarasoves@mail.ru>
2019-04-07 00:18:49 +0500

    grep ready

--- homeworks/grep/grep.py
+++ homeworks/grep/grep.py
@@ -1,17 +1,82 @@
-
 import argparse
 import sys
+import re
 
 
 def output(line):
     print(line)
 
 
+def output_line(line, pattern_in_line, count, line_number):
+    """
+    функция обрабатывает строку перед выводом, добавляет номер строки при необходимости
+    :param line: исходная строка
+    :param pattern_in_line: исходная строка соответствует шаблону поиска
+    :param count: номер строки
+    :param line_number: флаг, информирующий о необходимости нумеровать строки
+    :return: фозвращает строку для в формате вывода
+    """
+    if line_number:
+        if pattern_in_line:
+            return f'{count}:{line}'
+        else:
+            return f'{count}-{line}'
+    else:
+        return f'{line}'
+
+
+def pattern_in_line(pattern, line, invert, ignore_case):
+    """
+    функция проверяет соответствует ли строка шаблону
+    :param pattern: шаблон
+    :param line: исходная строка
+    :param invert: выводить строки, которые НЕ совпадают с шаблоном
+    :param ignore_case: не учитывать регистр
+    :return: возвращает булево значение
+    """
+    if ignore_case:
+        p_in_line = re.search(pattern.casefold(), line.casefold())
+    else:
+        p_in_line = re.search(pattern, line)
+
+    if not (invert ^ bool(p_in_line)):
+        return False
+    else:
+        return True
+
+
 def grep(lines, params):
+    count = 0  # считчик строк
+    count_pattern = 0  # счетчик строк, совпадающих с шаблоном
+    before_context = []  # список, для хранения строк не совпадающих с шаблоном, но которые тоже нужно будет выводить
+    after_context = 0  # количество строк, не совпадающих с шаблоном,которые осталось вывести после совпадения
     for line in lines:
         line = line.rstrip()
-        if params.pattern in line:
-            output(line)
+        count += 1
+        if pattern_in_line(params.pattern.replace('?', '.').replace('*', '.*'), line, params.invert, params.ignore_case):
+            count_pattern += 1
+            if not params.count:
+                before_context.reverse()
+                while before_context:
+                    output(output_line(before_context.pop(), False, count-len(before_context)-1, params.line_number))
+                output(output_line(line, True, count, params.line_number))
+                if params.context:
+                    after_context = params.context
+                elif params.after_context:
+                    after_context = params.after_context
+        elif bool(after_context) & (not params.count):
+            output(output_line(line, False, count, params.line_number))
+            after_context -= 1
+        elif bool(params.context | params.before_context) & (not params.count):
+            if params.context:
+                if len(before_context) == params.context:
+                    before_context.pop(0)
+            elif params.before_context:
+                if len(before_context) == params.before_context:
+                    before_context.pop(0)
+            before_context.append(line)
+    if params.count:
+        output(str(count_pattern))
 
 
 def parse_args(args):
